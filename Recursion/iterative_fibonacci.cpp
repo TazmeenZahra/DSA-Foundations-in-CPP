c051657b9da8221486746c0ab5b6ad8848c19dc6
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-04-20 12:59:07 +0500

    Create iterative_fibonacci.cpp

---
+++ Recursion/iterative_fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Iterative Fibonacci function
+int fibIterative(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    cout << "fib(11) using iteration = " << fibIterative(11) << endl;
+    return 0;
+}
